b7072c7be44d755049dd68a1a1285e8065ba7b2f
Guilherme <guiga.santos1711@gmail.com>
2020-04-21 18:52:07 -0300

    Classe de testes teminada

--- src/main/java/com/gsantos/ContaMagica.java
+++ src/main/java/com/gsantos/ContaMagica.java
@@ -18,14 +18,7 @@ public class ContaMagica {
     }
 	
     int getStatus(){
-        switch(status){
-            case PLATINUM:
-                return PLATINUM;
-            case GOLD:
-                return GOLD;
-            default:
-                return SILVER;
-        }
+        return status;
     }
 	
     void deposito(double valor) throws INVALID_OPER_EXCEPTION{
@@ -36,9 +29,11 @@ public class ContaMagica {
         switch(status){
             case PLATINUM:
                 saldo += valor * 1.25;
+                break;
             case GOLD:
                 saldo += valor * 1.1;
-                status = (saldo > 200000) ? GOLD : SILVER;
+                status = (saldo > 200000) ? PLATINUM : GOLD;
+                break;
             default:
                 saldo += valor;
                 status = (saldo > 50000) ? GOLD : SILVER;
@@ -50,14 +45,19 @@ public class ContaMagica {
         if (valor <= 0){
             throw new INVALID_OPER_EXCEPTION("Nao e possivel operar com valores >= 0");
         }
+        else if(valor > saldo){
+            throw new INVALID_OPER_EXCEPTION("Nao e possivel retirar valor maior que o saldo presente");
+        }
 
         switch(status){
             case PLATINUM:
                 saldo -= valor;
-                status = (saldo < 100000) ? SILVER : GOLD;
+                status = (saldo < 100000) ? GOLD : PLATINUM;
+                break;
             case GOLD:
                 saldo -= valor;
                 status = (saldo < 25000) ? SILVER : GOLD;
+                break;
             default:
                 saldo -= valor;
         }
--- src/test/java/com/gsantos/ContaMagicaTest.java
+++ src/test/java/com/gsantos/ContaMagicaTest.java
@@ -26,17 +26,6 @@ public class ContaMagicaTest
         c = new ContaMagica();
     }
 
-    @ParameterizedTest
-    @CsvSource({
-        "500, 0",
-        "60000, 1"
-    })
-    public void transicaoSilver(double valor, int estado) throws INVALID_OPER_EXCEPTION{
-        c.deposito(valor);
-        assertEquals(estado, c.getStatus());
-    }
-
-
     @Test
     public void depositoNegativo(){
         try {
@@ -55,5 +44,98 @@ public class ContaMagicaTest
         } catch (Exception e) {
             assertEquals("Nao e possivel operar com valores >= 0", e.getMessage());
         }
+    }
+
+    @Test
+    public void retiradaMaiorQueSaldo(){
+        try {
+            c.retirada(100);
+            fail("Shold show an error");   
+        } catch (Exception e) {
+            assertEquals("Nao e possivel retirar valor maior que o saldo presente", e.getMessage());
+        }
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+        "500.0, 0",
+        "60000.0, 1",
+        "300000.0, 1"
+    })
+    public void transicaoSilverDeposito(double valor, int estado) throws INVALID_OPER_EXCEPTION{
+        c.deposito(valor);            
+
+        assertEquals(estado, c.getStatus());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "500.0, 1",
+        "200000.0, 2",
+    })
+    public void transicaoGoldDeposito(double valor, int estado) throws INVALID_OPER_EXCEPTION{
+        // Deposito para ir para gold
+        c.deposito(60000);
+
+        c.deposito(valor);
+
+        assertEquals(estado, c.getStatus());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "100000, 2",
+        "150000, 1",
+        "200000, 1",
+    })
+    public void transicaoPlatinumRetirada(double valor, int estado) throws INVALID_OPER_EXCEPTION{
+        // Deposito para ir para platinum
+        c.deposito(60000);
+        c.deposito(150000);
+
+        c.retirada(valor);
+
+        assertEquals(estado, c.getStatus());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "150, 1",
+        "50000, 0",
+    })
+    public void transicaoGoldRetirada(double valor, int estado) throws INVALID_OPER_EXCEPTION{
+        // Deposito para ir para platinum
+        c.deposito(60000);
+
+        c.retirada(valor);
+
+        assertEquals(estado, c.getStatus());
+    }
+
+    @Test
+    public void bonusDepositoGold() throws INVALID_OPER_EXCEPTION{
+        c.deposito(60000);
+        c.deposito(20000);
+
+        double saldoEsperado = 60000 + (20000 * 1.1);
+
+        assertEquals(saldoEsperado, c.getSaldo());
+    }
+
+    @Test
+    public void bonusDepositoPlatinum() throws INVALID_OPER_EXCEPTION{
+        c.deposito(60000);
+        c.deposito(150000);
+
+        System.out.println(c.getSaldo());
+        // Saldo ate chegar no Platinum
+        double saldoEsperado = 60000 + (150000 * 1.1);
+
+        c.deposito(50000);
+
+        saldoEsperado += (50000 * 1.25);
+
+        assertEquals(saldoEsperado, c.getSaldo());
     }
 }
