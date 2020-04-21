de076e8ca7312465dfa1ae6d81a0bbe968ba5673
Guilherme <guiga.santos1711@gmail.com>
2020-04-21 15:07:59 -0300

    adição de testes para valores <= 0

--- src/main/java/com/gsantos/ContaMagica.java
+++ src/main/java/com/gsantos/ContaMagica.java
@@ -5,9 +5,14 @@ public class ContaMagica {
     public static final int GOLD = 1;
     public static final int PLATINUM = 2;
     
-    private static double  saldo;
+    private static double saldo;
     private static int status;
     
+    public ContaMagica(){
+        saldo = 0;
+        status = SILVER;
+    }
+
     double getSaldo(){
         return saldo;
     }
@@ -23,13 +28,38 @@ public class ContaMagica {
         }
     }
 	
-    void deposito(int valor) throws INVALID_OPER_EXCEPTION{
+    void deposito(double valor) throws INVALID_OPER_EXCEPTION{
         if (valor <= 0){
-            throw new INVALID_OPER_EXCEPTION("Nao e possivel depositar valores >= 0");
+            throw new INVALID_OPER_EXCEPTION("Nao e possivel operar com valores >= 0");
         }
+
+        switch(status){
+            case PLATINUM:
+                saldo += valor * 1.25;
+            case GOLD:
+                saldo += valor * 1.1;
+                status = (saldo > 200000) ? GOLD : SILVER;
+            default:
+                saldo += valor;
+                status = (saldo > 50000) ? GOLD : SILVER;
+        }
+
     }
 
-    void retirada(int valor) throws INVALID_OPER_EXCEPTION{
+    void retirada(double valor) throws INVALID_OPER_EXCEPTION{
+        if (valor <= 0){
+            throw new INVALID_OPER_EXCEPTION("Nao e possivel operar com valores >= 0");
+        }
 
+        switch(status){
+            case PLATINUM:
+                saldo -= valor;
+                status = (saldo < 100000) ? SILVER : GOLD;
+            case GOLD:
+                saldo -= valor;
+                status = (saldo < 25000) ? SILVER : GOLD;
+            default:
+                saldo -= valor;
+        }
     }
 }
--- src/test/java/com/gsantos/ContaMagicaTest.java
+++ src/test/java/com/gsantos/ContaMagicaTest.java
@@ -3,18 +3,57 @@ package com.gsantos;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.omg.CORBA.INITIALIZE;
+import org.junit.jupiter.api.parallel.ResourceLock;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+//import org.junit.jupiter.*;
+//import org.omg.CORBA.INITIALIZE;
 
 /**
  * Unit test for simple App.
  */
 public class ContaMagicaTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private ContaMagica c;
+
+    @BeforeEach
+    public void initialize(){
+        c = new ContaMagica();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "500, 0",
+        "60000, 1"
+    })
+    public void transicaoSilver(double valor, int estado) throws INVALID_OPER_EXCEPTION{
+        c.deposito(valor);
+        assertEquals(estado, c.getStatus());
+    }
+
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void depositoNegativo(){
+        try {
+            c.deposito(-100);
+            fail("Shold show an error");   
+        } catch (Exception e) {
+            assertEquals("Nao e possivel operar com valores >= 0", e.getMessage());
+        }
+    }
+
+    @Test
+    public void retiradaNegativa(){
+        try {
+            c.retirada(-100);
+            fail("Shold show an error");   
+        } catch (Exception e) {
+            assertEquals("Nao e possivel operar com valores >= 0", e.getMessage());
+        }
     }
 }
